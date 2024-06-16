ce64869b0f7bce1879bb3308e2078a6f6f889f0e
soywiz <soywiz@gmail.com>
2024-06-16 08:00:20 +0200

    Make local installation for korge-forge on windows

--- install-korge-forge.cmd
+++ install-korge-forge.cmd
@@ -1,45 +1,49 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 
-set KORGE_FORGE_VERSION=v0.1.1
-echo KorGE Forge Installer %KORGE_FORGE_VERSION%
+SET KORGE_FORGE_VERSION=v0.1.1
+ECHO KorGE Forge Installer %KORGE_FORGE_VERSION%
 
-set JRE_LOCAL=%LOCALAPPDATA%\KorgeForgeInstaller
+REM SET INSTALLER_PATH=%LOCALAPPDATA%\KorgeForgeInstaller
+SET INSTALLER_PATH=.\korge-forge-installer
 
-set INSTALLER_URL=https://github.com/korlibs/korge-forge-installer/releases/download/%KORGE_FORGE_VERSION%/korge-forge-installer.jar
-set INSTALLER_SHA1=8b29794bbc14e50f7c4d9c4b673aabbbdcf6cfd1
-set INSTALLER_LOCAL_FILE=korge-forge-installer-%KORGE_FORGE_VERSION%.jar
-set INSTALLER_LOCAL=%JRE_LOCAL%\%INSTALLER_LOCAL_FILE%
+SET INSTALLER_URL=https://github.com/korlibs/korge-forge-installer/releases/download/%KORGE_FORGE_VERSION%/korge-forge-installer.jar
+SET INSTALLER_SHA1=8b29794bbc14e50f7c4d9c4b673aabbbdcf6cfd1
+SET INSTALLER_LOCAL_FILE=korge-forge-installer-%KORGE_FORGE_VERSION%.jar
+SET INSTALLER_LOCAL=%INSTALLER_PATH%\%INSTALLER_LOCAL_FILE%
 
-set JRE_URL=https://github.com/korlibs/universal-jre/releases/download/0.0.1/OpenJDK21U-jre_x64_windows_hotspot_21.0.3_9.zip
-REM set JRE_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.3%2B9/OpenJDK21U-jre_x64_windows_hotspot_21.0.3_9.zip
-set JRE_SHA1=0a36d67c443387bab59e335b8a073d7d0f3a9575
-set JRE_ZIP=%JRE_LOCAL%\jre.zip
-set JRE_JAVA_BIN=%JRE_LOCAL%\jdk-21.0.3+9-jre\bin
+SET JRE_URL=https://github.com/korlibs/universal-jre/releases/download/0.0.1/OpenJDK21U-jre_x64_windows_hotspot_21.0.3_9.zip
+REM SET JRE_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.3%2B9/OpenJDK21U-jre_x64_windows_hotspot_21.0.3_9.zip
+SET JRE_SHA1=0a36d67c443387bab59e335b8a073d7d0f3a9575
+SET JRE_ZIP=%INSTALLER_PATH%\jre.zip
+SET JRE_JAVA_BIN=%INSTALLER_PATH%\jdk-21.0.3+9-jre\bin
 
 REM https://github.com/adoptium/temurin21-binaries/releases
 
-echo Temporary files at: %JRE_LOCAL%
+ECHO "Temporary files at: %INSTALLER_PATH%"
 
-mkdir "%JRE_LOCAL%" 2> NUL
-
-SET DOWNLOAD_URL=%JRE_URL%
-SET DOWNLOAD_LOCAL=%JRE_ZIP%
-SET DOWNLOAD_SHA1=%JRE_SHA1%
-CALL :DOWNLOAD_FILE
+MKDIR "%INSTALLER_PATH%" 2> NUL
 
 SET DOWNLOAD_URL=%INSTALLER_URL%
 SET DOWNLOAD_LOCAL=%INSTALLER_LOCAL%
 SET DOWNLOAD_SHA1=%INSTALLER_SHA1%
 CALL :DOWNLOAD_FILE
 
-if not exist "%JRE_JAVA_BIN%" (
-    echo Unzipping %JRE_ZIP%...
-    tar -xf "%JRE_ZIP%" -C "%JRE_LOCAL%" 2> NUL > NUL
-    echo Ok
+IF NOT EXIST "%JRE_JAVA_BIN%" (
+    SET DOWNLOAD_URL=%JRE_URL%
+    SET DOWNLOAD_LOCAL=%JRE_ZIP%
+    SET DOWNLOAD_SHA1=%JRE_SHA1%
+    CALL :DOWNLOAD_FILE
+
+    ECHO Unzipping %JRE_ZIP%...
+    tar -xf "%JRE_ZIP%" -C "%INSTALLER_PATH%" 2> NUL > NUL
+    DEL %JRE_ZIP%
+    ECHO Ok
 )
 
-"%JRE_JAVA_BIN%\java" -jar %INSTALLER_LOCAL% %*
+CD %INSTALLER_PATH%
+"..\%JRE_JAVA_BIN%\java" -jar "..\%INSTALLER_LOCAL%" %*
+CD ..
 
 EXIT /B
 
@@ -51,21 +55,21 @@ EXIT /B
     SET DOWNLOAD_LOCAL_TMP=%DOWNLOAD_LOCAL%.tmp
     SET /A SHA1_RETRIES=3
 
-    if not exist "%DOWNLOAD_LOCAL%" (
-        echo Downloading %DOWNLOAD_URL% into: %DOWNLOAD_LOCAL_TMP%
+    IF NOT EXIST "%DOWNLOAD_LOCAL%" (
+        ECHO Downloading %DOWNLOAD_URL% into: %DOWNLOAD_LOCAL_TMP%
         REM curl -s -L "%DOWNLOAD_URL%" -o "%DOWNLOAD_LOCAL_TMP%" && timeout /T 1 /NOBREAK > NUL
         powershell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%DOWNLOAD_LOCAL_TMP:\=\\%')"
         REM powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-BitsTransfer -Source '%DOWNLOAD_URL%' -Destination '%DOWNLOAD_LOCAL_TMP:\=\\%'"
 
         IF NOT "%DOWNLOAD_SHA1%"=="" (
             :REPEAT_SHA1
-            echo Computing hash...
+            ECHO Computing hash...
             FOR /f %%i IN (
                 'powershell -NoProfile -ExecutionPolicy Bypass -Command "(Get-Filehash -Path '%DOWNLOAD_LOCAL_TMP:\=\\%' -Algorithm SHA1).Hash"'
             ) DO SET SHA1=%%i
 
             IF NOT "%SHA1_RETRIES%"=="0" (
-                if /i NOT "%SHA1%"=="%DOWNLOAD_SHA1%" (
+                IF /i NOT "%SHA1%"=="%DOWNLOAD_SHA1%" (
                     ECHO "File not ready, retrying in 2 seconds (retries %SHA1_RETRIES%) %DOWNLOAD_LOCAL_TMP:\=\\%"
                     TIMEOUT /T 2 /NOBREAK > NUL
                     SET /A SHA1_RETRIES-=1
@@ -73,7 +77,7 @@ EXIT /B
                 )
             )
 
-            if /i NOT "%SHA1%"=="%DOWNLOAD_SHA1%" (
+            IF /i NOT "%SHA1%"=="%DOWNLOAD_SHA1%" (
                 ECHO "Error downloading file expected '%DOWNLOAD_SHA1%' but found '%SHA1%' from url %DOWNLOAD_URL%"
                 ECHO "URL: %DOWNLOAD_URL%"
                 ECHO "SHA1: %DOWNLOAD_SHA1%"
@@ -83,6 +87,6 @@ EXIT /B
         )
 
         MOVE "%DOWNLOAD_LOCAL_TMP%" "%DOWNLOAD_LOCAL%" 2> NUL > NUL
-        echo Ok
+        ECHO Ok
     )
 EXIT /B
